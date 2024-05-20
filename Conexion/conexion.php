fe4166d4d234896fdde863fde0fef536e978cc6d
Sofia15M <sofiaortizacevedo2007@gmail.com>
2024-05-20 17:15:29 -0500

    Se actualiza la conecion a la base de datos

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -5,5 +5,5 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$db = mysqli_select_db($connection, "v_014_digitalface");
+$db = mysqli_select_db($connection, "v_015_digitalface");
 ?>
